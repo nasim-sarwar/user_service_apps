26f18af2a30cadf5919f577390ec9b825bb61089
Nasim Sarwar <nasimsarwar321@gmail.com>
2025-11-26 17:19:12 -0500

    git commit -m "project commit"

---
+++ src/test/java/com/javadeveloperblogs/user_service_apps/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadeveloperblogs.user_service_apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
