a56be9533f269448973b3772a32b0247a513c9e7
Nasim Sarwar <nasimsarwar321@gmail.com>
2025-12-10 22:24:56 -0500

    code updated

--- src/main/java/com/javadeveloperblogs/app/ws/io/Repository/AddressRepository.java
+++
@@ -1,4 +0,0 @@
-package com.javadeveloperblogs.app.ws.io.Repository;
-
-public interface AddressRepository {
-}
--- src/main/java/com/javadeveloperblogs/app/ws/io/entity/RefreshTokenEntity.java
+++
@@ -1,4 +0,0 @@
-package com.javadeveloperblogs.app.ws.io.entity;
-
-public class RefreshTokenEntity {
-}
